fb77ab3b9aa8248ba8c5f24f13d428add7d186c2
sfave <sebpfave@gmail.com>
2020-12-10 22:19:04 -0500

    Demo email sending

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import smtplib
-from parameters import password
+import parameters as pr
 
-myemail = ""
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()  # secures connection
-connection.login(user=myemail, password=password)
+connection.login(user=pr.my_email, password=pr.password)
+connection.sendmail(from_addr=pr.my_email,
+                    to_addrs=pr.to_email,
+                    msg="Subject: Hello\n\nThis is the email body")
+connection.close()
