4c14ad815087add2f5adfad203cc37b30edbac19
sfave <sebpfave@gmail.com>
2020-12-10 22:20:58 -0500

    connection using with refactor

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import smtplib
 import parameters as pr
 
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()  # secures connection
-connection.login(user=pr.my_email, password=pr.password)
-connection.sendmail(from_addr=pr.my_email,
-                    to_addrs=pr.to_email,
-                    msg="Subject: Hello\n\nThis is the email body")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()  # secures connection
+    connection.login(user=pr.my_email, password=pr.password)
+    connection.sendmail(
+        from_addr=pr.my_email,
+        to_addrs=pr.to_email,
+        msg="Subject: Hello\n\nThis is the email body")
