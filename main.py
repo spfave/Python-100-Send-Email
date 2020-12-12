3b2150e95d4c884431cdfb1369cbd27773990470
sfave <sebpfave@gmail.com>
2020-12-11 19:12:44 -0500

    Basic datetime module

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import smtplib
+""" import smtplib
 import parameters as pr
 
 
@@ -9,3 +9,15 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
         from_addr=pr.my_email,
         to_addrs=pr.to_email,
         msg="Subject: Hello\n\nThis is the email body")
+ """
+
+
+import datetime as dt
+
+
+now = dt.datetime.now()
+print(now)
+
+
+date_of_birth = dt.datetime(year=1989, month=1, day=18)
+print(date_of_birth)
