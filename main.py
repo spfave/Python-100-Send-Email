86a4fca91385e01be0d301128efda3ad5a856ac2
sfave <sebpfave@gmail.com>
2020-12-11 20:25:44 -0500

    Get date

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import datetime as dt
+import random
+import pandas as pd
+import smtplib
+
+
+# Constants
+file_birthdays = "data/birthdays.csv"
+
+
+# Classes
+class BirthdayMailer():
+    """  """
+
+    def __init__(self):
+        """  """
+        self.get_date()
+
+    def get_date(self):
+        self.date = dt.datetime.now().date()
+
+
+# Main
+birthday_mailer = BirthdayMailer()
+print(birthday_mailer.date)
+
 ##################### Extra Hard Starting Project ######################
 
 # 1. Update the birthdays.csv
@@ -7,7 +33,3 @@
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
